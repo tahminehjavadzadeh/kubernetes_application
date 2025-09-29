fa7b0a718738bafabe15540d940126b24f7f0b0c
Tahmineh <tamin.j.cs@gmail.com>
2025-09-29 21:01:01 +0200

    change 2

--- test.py
+++ test.py
@@ -4,3 +4,11 @@ x = 10
 print(x==10)
 print(x==11)
 print(x>12)
+print("\n lets make it more fun")
+Name= "tamin"
+age= 33
+if Name=="tamin" and age==33:
+    
+    print()
+    age = 32
+    print ("your name is", Name, " and you are ",age ,"years old")
